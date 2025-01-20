d633c92ee0edf53137ff9dca38e42a75e6ed4823
DishaKD21 <vdsh321@gmail.com>
2025-01-20 19:52:15 +0530

    added linkedlist sol

---
+++ LinkedList/Medium Problems LL/876.Middle of the Linked List.cpp
@@ -0,0 +1,30 @@
+//876.Middle of the Linked List
+/**making two pointers both are pointing to head and one is moving two steps ahead and one is moving one steps ahead
+when fast pointer will reach to the end at that time slow one will reach at middle
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* temp1 = head;
+        ListNode* temp2 = head;
+
+        while (temp2 != nullptr && temp2->next != nullptr) {
+            temp1 = temp1->next;
+            temp2 = temp2->next->next;
+        }
+
+        return temp1;
+    }
+};
+
+    
