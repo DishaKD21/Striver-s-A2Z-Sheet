d5a7776a30c721a46442c511bbf30a74cbd213cd
DishaKD21 <vdsh321@gmail.com>
2025-02-21 21:02:08 +0530

    added

---
+++ Learn The Basics/Things to know in c++/Decision Making in c++.cpp
@@ -0,0 +1,17 @@
+//Decision Making in c++
+//apply simple if else
+class Solution {
+  public:
+    string compareNM(int n, int m){
+      if(n<m){
+          cout<<"lesser";
+      }
+      else if(n>m){
+          cout<<"greater";
+      }
+      else{
+          cout<<"equal";
+      }
+      return "";
+    }
+};
