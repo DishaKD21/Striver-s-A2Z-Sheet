585a03a3c8aa5bc1d199b63780c978b59b0037ed
DishaKD21 <vdsh321@gmail.com>
2025-03-04 00:30:16 +0530

    changed

--- LinkedList/singly LinkedList/237. Delete Node in a Linked List.cpp
+++ LinkedList/singly LinkedList/237. Delete Node in a Linked List.cpp
@@ -1,5 +1,9 @@
 //237. Delete Node in a Linked List
-
+/*
+as head is not given to us we will just take the value of given node and we will 
+overwrite the value of it with next node's val so now both the node will have same values now we can unlinked
+or make the next node unreachable 
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,13 +12,9 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-
-/*
-
-*/
 class Solution {
 public:
-    void deleteNode(ListNode* node) {
+     void deleteNode(ListNode* node) {
         node->val=node->next->val;
         node->next =node->next->next;    }
 };
