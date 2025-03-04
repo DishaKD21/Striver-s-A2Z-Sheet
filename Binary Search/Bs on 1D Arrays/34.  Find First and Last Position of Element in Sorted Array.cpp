d316c4d421e6aeeb60dea0ad8476003ed838e428
DishaKD21 <vdsh321@gmail.com>
2025-03-04 23:52:03 +0530

    added

---
+++ Binary Search/Bs on 1D Arrays/34.  Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,25 @@
+//34. Find First and Last Position of Element in Sorted Array
+/*
+we are initializing two indexes starting and ending position and running forloop from starting and ending 
+and breaking loop when element is equal to target and saving it's index.
+*/
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int startingPosition = -1, endingPosition = -1;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            if(nums[i] == target){
+                startingPosition = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>=0; i--){
+            if(nums[i] == target){
+                endingPosition = i;
+                break;
+            }
+        }
+        return {startingPosition, endingPosition};
+    }
+};
